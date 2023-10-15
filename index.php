01c5d1a32d23eaf7f2549c1e452ef6488efc5e06
Wesaid <carbophoss@gmail.com>
2023-10-15 20:03:38 +0300

    New

---
+++ index.php
@@ -0,0 +1,28 @@
+
+<html>
+    <head>
+        <title> Калькулятор</title>
+    </head>
+    <body>
+<form  method="post" class = "calculate">
+
+    <label for="kzt">Цена за игру в стим:</label>
+    <input id="kzt" name="game" type="number"/>
+    <input name="send" type="submit" value="Посчитать">
+
+</form>
+    <?php
+    if($_POST["send"])
+        {
+            if(isset($_POST["game"]))
+            {
+                $game = $_POST['game']; }
+                $result = (($game / 100) * 9) + $game;
+                echo $result;
+        }
+    $q = 0.2102;
+
+    ?>
+
+    </body>
+</html>
