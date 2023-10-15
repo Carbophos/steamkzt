37aea56b0ef28fff228f2424d01eb1ac5a0f5f88
Wesaid <carbophoss@gmail.com>
2023-10-15 21:31:53 +0300

    Всё починил

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-
-<html lang="eu">
+<html lang="ru">
     <head>
         <title> Калькулятор</title>
     </head>
@@ -7,22 +6,22 @@
 <form  method="post" class = "calculate">
 
     <label for="kzt">Цена за игру в стим:</label>
-    <input id="kzt" name="game" type="number"/>
+    <input id="kzt" name="game" type="number" min="1"/>
     <input name="send" type="submit" value="Посчитать">
 
 </form>
     <?php
-    if($_POST["send"])
-        {
-            if(isset($_POST["game"]))
+    //if($_POST['game']) { $game = strip_tags($_POST['game'] ); }
+    if(isset($_POST['send']))
+    {
+            if(isset($_POST['game']))
             {
-                $game = $_POST['game']; }
+                $game = $_POST['game'];
                 $result = (($game / 100) * 9) + $game;
-                echo $result;
-        }
+                echo "Итог сколько нужно положить: ".$result. "₸";
+            }
+    }
     $q = 0.2102;
-echo 'haha';
     ?>
-
     </body>
 </html>
